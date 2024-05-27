f47ce4666ff11f814045f8f162ccd893066dfb99
Roger5791 <sauravantony2002@gmail.com>
2024-05-27 20:40:58 +0530

    Third-Commit

--- src/Home/Components/Deals/Deals.jsx
+++ src/Home/Components/Deals/Deals.jsx
@@ -4,34 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Deals = () => {
 
-  const countdown = () => {
-    // Specify the date and time we are counting down to.
-    const countDate = new Date("Dec 1, 2024 22:37:43").getTime();
-    const now = new Date().getTime();
-    const remainingTime = ((countDate) - (now));
-  
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const textDay = Math.floor(remainingTime / day);
-    const textHour = Math.floor((remainingTime % day) / hour);
-    const textMinute = Math.floor((remainingTime % hour) / minute);
-    const textSecond = Math.floor((remainingTime % minute) / second);
-  
-    document.querySelector(".day").innerText = textDay > 0 ? textDay : 0;
-    document.querySelector(".hour").innerText = textHour > 0 ? textHour : 0;
-    document.querySelector(".minute").innerText = textMinute > 0 ? textMinute : 0;
-    document.querySelector(".second").innerText = textSecond > 0 ? textSecond : 0;
-    document.querySelector(".day2").innerText = textDay > 0 ? textDay : 0;
-    document.querySelector(".hour2").innerText = textHour > 0 ? textHour : 0;
-    document.querySelector(".minute2").innerText = textMinute > 0 ? textMinute : 0;
-    document.querySelector(".second2").innerText = textSecond > 0 ? textSecond : 0;
-  };
-  
-  // should use 500 as setInterval won't always run on time.
- setInterval(countdown); 
+   
                     
 
 
@@ -138,7 +111,34 @@ const Deals = () => {
   )
 
  
- 
+  const countdown = () => {
+    // Specify the date and time we are counting down to.
+    const countDate = new Date("Dec 1, 2024 22:37:43").getTime();
+    const now = new Date().getTime();
+    const remainingTime = ((countDate) - (now));
+  
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const textDay = Math.floor(remainingTime / day);
+    const textHour = Math.floor((remainingTime % day) / hour);
+    const textMinute = Math.floor((remainingTime % hour) / minute);
+    const textSecond = Math.floor((remainingTime % minute) / second);
+  
+    document.querySelector(".day").innerText = textDay > 0 ? textDay : 0;
+    document.querySelector(".hour").innerText = textHour > 0 ? textHour : 0;
+    document.querySelector(".minute").innerText = textMinute > 0 ? textMinute : 0;
+    document.querySelector(".second").innerText = textSecond > 0 ? textSecond : 0;
+    document.querySelector(".day2").innerText = textDay > 0 ? textDay : 0;
+    document.querySelector(".hour2").innerText = textHour > 0 ? textHour : 0;
+    document.querySelector(".minute2").innerText = textMinute > 0 ? textMinute : 0;
+    document.querySelector(".second2").innerText = textSecond > 0 ? textSecond : 0;
+  };
+  
+  // should use 500 as setInterval won't always run on time.
+ setInterval(countdown); 
 
 }
 
